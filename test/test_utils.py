760a4b930b3623db9cd501b549333eb6198d15fc
Manuel Cortez <manuel@manuelcortez.net>
2022-02-19 14:28:31 -0600

    Added tests to other sizes in utils module

--- test/test_utils.py
+++ test/test_utils.py
@@ -3,8 +3,12 @@ from updater import utils
 
 @pytest.mark.parametrize("number_of_bytes, expected_result", [
         (11, "11"),
+        (200000, "195.31Kb"),
+        (2000000, "1.91Mb"),
         (20000000, "19.07Mb"),
         (2000000000, "1.86Gb"),
+        (2000000000000, "1.82Tb"),
+        (2000000000000000, "1.78Pb"),
         ])
 def test_convert_bytes(number_of_bytes, expected_result):
     result = utils.convert_bytes(number_of_bytes)
