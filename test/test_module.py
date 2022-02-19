14324e56fe035c28b3bb40592f4076e09dfb3f33
Manuel Cortez <manuel@manuelcortez.net>
2022-02-19 14:14:53 -0600

    Increased coverage of test module

--- test/test_module.py
+++ test/test_module.py
@@ -3,14 +3,14 @@ import pytest
 import updater
 from unittest import mock
 
-@pytest.mark.parametrize("system", [("Windows", "Linux", "Darwin")])
+@pytest.mark.parametrize("system", [("Windows"), ("Linux"), ("Darwin")])
 def test_find_datafiles(system):
     with mock.patch("platform.system", return_value=system):
         result = updater.find_datafiles()
-        if system == "Windows":
-            assert "bootstrap.exe" in result[0][1]
-            assert os.path.exists(result[0][1][0])
-        else:
-            assert len(result[0][1]) == 2
-            assert os.path.exists(result[0][1][0])
-            assert os.path.exists(result[0][1][1])
+    if system == "Windows":
+        assert "bootstrap.exe" in result[0][1][0]
+        assert os.path.exists(result[0][1][0])
+    else:
+        assert len(result[0][1]) == 2
+        assert os.path.exists(result[0][1][0])
+        assert os.path.exists(result[0][1][1])
